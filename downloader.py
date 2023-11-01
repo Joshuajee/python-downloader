d30b405d73d7dc542c1cebc0305f0e77a19a7631
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-11-01 10:19:52 +0100

    Removed print statement

--- downloader.py
+++ downloader.py
@@ -3,8 +3,6 @@ import requests
 
 def downloader(url, ext):
     response = requests.get(url)
-    # Check if the request was successful
-    print(response.status_code)
     if response.status_code == 200:
         return (response.content, ext)
     else: 
